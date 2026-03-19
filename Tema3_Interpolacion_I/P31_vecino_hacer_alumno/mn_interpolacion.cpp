a1a7446b8fb5e65101291f6ffcd729d39d326995
Pabloflopygame <pabloflopygame@gmail.com>
2026-03-19 03:29:43 +0000

    Terminado tema 3.1 y 3.2

--- Tema3_Interpolacion_I/P31_vecino_hacer_alumno/mn_interpolacion.cpp
+++ Tema3_Interpolacion_I/P31_vecino_hacer_alumno/mn_interpolacion.cpp
@@ -8,7 +8,73 @@ Array1D<real> &f, /// valores de función en los puntos de interpolación
 real x0)  /// punto donde se evalua la función interpolada
 {
   /// HACER ALUMNO
+    /// Recordemos que programamos usando ingles por las limitaciones de
+    /// variables al no poder usar ñ o tildes, pero comentaremos en Español.
 
+    // Casos exepcionales.
+    int sizex = x.dim1();
+    int sizef = f.dim1();
+    // inputs inválidos:
+    // vacios (facilmente puede ocurrir)
+    // negativos, no tiene sentido en nuestro uso,
+    // pero es por si modifican cosas avanzadas de c++.
+    //
+    // *Los juntamos en 1 para evitar un if de "vacios" y otro de "negativos".
+    if ((sizex <= 0) || (sizef <= 0)) {
+        return -1;
+    }
+    // dimensiones diferentes
+    if (sizex != sizef) {
+        return -1;
+    }
+
+    // casos extremos
+    // Se podría descartar el de la derecha y ponerlo al final del bucle,
+    // pero nos dejaría abiertos a errores de falta de ordenación y no retornar
+    // un código de error a su vez que sería lento eficientemente hablando O(n)
+    // para un caso extremo sin ninguna necesidad.
+    // extrema izquierda
+    if (x0 <= x[0]) {
+        return f[0];
+    }
+    // extrema derecha
+    if (x0 >= x[sizex-1]) {
+        return f[sizef-1];
+    }
+
+    // Casos normales.
+    // Vamos a recorrer de izquierda a derecha.
+    // x[0]-x[1] / x[1]-x[2] / ... / x[n-1] - x[n]
+    // y ver cual contiene a x0.
+    for(int i=0; i < sizex-1; i++){
+        // si está entre el intervalo lo aproximamos al más cercano.
+        if ((x0 >= x[i]) && (x0 <= x[i+1])) {
+            // no usamos mn_distancia porque tiene problemas cuando el valor es central.
+            // al normalizar en esa función se pierde precición y genera ruido decimal.
+            real distance_left = fabs(x[i]-x0);
+            real distance_right = fabs(x0-x[i+1]);
+
+            // aquí sientanse libres de quitar o dejar el = para la política que deseén.
+            // actualmente acotamos a la izquierda en caso de estar en el medio
+            // (para que encaje con los resultados calculados en la verificación).
+            if (distance_left <= distance_right) {
+                return f[i];
+            } else{
+                return f[i+1];
+            }
+        }
+    }
+
+    // caso de error no esperado;
+    // Probablemente x no esté ordenado.
+    return -1;
+    // en 3.7 explica la verificación con el estudio (contexto) en el que se usa.
+    // en este caso se nos simplifica considerablemente ya que el main tiene precalculados
+    // las interpolaciones con este métodos guardados y simplemente estamos comparando.
+    // Pero el contexto es meramente ver el numero de fallecimientos por grupo de edad, que si
+    // miramos en la gráfica real vemos que está en valores razonables dado a como es el algoritmo.
+    //
+    // *Recuerda que en el examen esto sobra, porque iría en el documento. Esto lo pongo para estudiar.
 }
 
 
--- Tema3_Interpolacion_I/P32_lineal_hacer_alumno/mn_interpolacion.cpp
+++ Tema3_Interpolacion_I/P32_lineal_hacer_alumno/mn_interpolacion.cpp
@@ -3,12 +3,76 @@
 
 /// INTERPOLACION LINEAL
 real mn_interpolacion_lineal(
-Array1D<real> &x, /// puntos de interpolaci�n
-Array1D<real> &f, /// valores de funci�n en los puntos de interpolaci�n
-real x0)  /// punto donde se evalua la funci�n interpolada
+Array1D<real> &x, /// puntos de interpolación
+Array1D<real> &f, /// valores de función en los puntos de interpolación
+real x0)  /// punto donde se evalua la función interpolada
 {
   /// HACER ALUMNO
+    // Casos exepcionales.
+    int sizex = x.dim1();
+    int sizef = f.dim1();
+    // inputs inválidos:
+    // vacios (facilmente puede ocurrir)
+    // negativos, no tiene sentido en nuestro uso,
+    // pero es por si modifican cosas avanzadas de c++.
+    //
+    // *Los juntamos en 1 para evitar un if de "vacios" y otro de "negativos".
+    if ((sizex <= 0) || (sizef <= 0)) {
+        return -1;
+    }
+    // dimensiones diferentes
+    if (sizex != sizef) {
+        return -1;
+    }
 
+    // casos extremos
+    // extrema izquierda (se toma primer intervalo)
+    if (x0 <= x[0]) {
+        return f[0] + ((f[0+1] - f[0])/(x[0+1] - x[0]))*(x0-x[0]);;
+    }
+    // extrema derecha (se toma último intervalo)
+    if (x0 >= x[sizex-1]) {
+        return f[sizex-2] + ((f[sizex-1] - f[sizex-2])/(x[sizex-1] - x[sizex-2]))*(x0-x[sizex-2]);;
+    }
+
+    // Casos normales.
+    // Vamos a recorrer de izquierda a derecha.
+    // x[0]-x[1] / x[1]-x[2] / ... / x[n-1] - x[n]
+    // y ver cual contiene a x0.
+    for(int i=0; i < sizex-1; i++){
+        // si está entre el intervalo calculamos la recta
+        if ((x0 >= x[i]) && (x0 <= x[i+1])) {
+            // comprobamos si tenemos el valor exacto antes de interpolar.
+            // lo hacemos dentro del intervalo para evitar operaciones inecesarias del loop.
+            if (x0 == x[i]){
+                return f[i];
+            }
+            if (x0 == x[i+1]){
+                return f[i+1];
+            }
+
+            // aquí para simplificar simplemente vamos a usar la formula
+            // que calcula la recta y a su vez calcula el punto para esta.
+            // Pi(x) = f(xi) + ((f(xi+1) − f(xi))/(xi+1 − xi)) * (x−xi).
+
+            return f[i] + ((f[i+1] - f[i])/(x[i+1] - x[i]))*(x0-x[i]);
+        }
+    }
+
+    // caso de error no esperado;
+    // Probablemente x no esté ordenado.
+    // si f no está ordenado retornaría un valor (aunque incorrecto), no llegaría a -1
+    // es x el que causa que el recorrido falle y pueda llegar aquí.
+    return -1;
+    // En esta verificación aparte de lo de epidemeología que lo hemos mencionado en ejercicios anteriores
+    // lo han aplicado en un vector de 4 puntos y han interpolado para añadir más definición.
+    // con este método esperamos lo que hacemos, crear montón de puntos intermedios.
+    // Muy buena herramienta si lo que queremos hacer es subdividir un segmento y usada en trabajos vectoriales!
+    // exactamente lo que esperamos y con una utilidad muy real.
+    //
+    // Ha mencionar el proceso de creación tiene que retornar 0 para que se haya hecho de forma correcta.
+    // a su vez el tiempo debe ser lógico, 0.08 segundos es esperable (algo lento, pero esperable al tener
+    // leer y escribir de disco en ficheros).
 }
 
 
